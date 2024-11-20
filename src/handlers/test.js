a24a9d6cdede347e1fb02c8b6b8fe0c7c6374406
nurdworker <nurdworker@gmail.com>
2024-11-20 15:55:46 +0900

    after cicd 2024 11 20

--- src/handlers/test.js
+++ src/handlers/test.js
@@ -1,14 +1,54 @@
-exports.handler = async (event) => {
-  // Example response
-  const response = {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*", // 모든 출처 허용
-      "Access-Control-Allow-Methods": "GET,OPTIONS", // 허용하는 메서드
-      "Access-Control-Allow-Headers": "Content-Type", // 허용하는 헤더
-    },
-    body: JSON.stringify({ message: "hello from test lambda" }),
-  };
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const user = process.env.MONGODB_USER;
+const password = process.env.MONGODB_PASSWORD;
+const host = process.env.MONGODB_HOST;
+const port = process.env.MONGODB_PORT;
+const dbName = process.env.MONGODB_DB;
 
-  return response;
+exports.handler = async (event) => {
+  console.log("invoke!!여기야1");
+  const requestType = event.queryStringParameters?.request;
+  const encodedPassword = encodeURIComponent(password);
+  const uri = `mongodb://${user}:${encodedPassword}@${host}:${port}/${dbName}`;
+  console.log(uri);
+  let client;
+
+  if (requestType === "connectDb") {
+    try {
+      client = new MongoClient(uri);
+      await client.connect();
+      const db = client.db(dbName);
+      const collections = await db.listCollections().toArray();
+      const collectionNames = collections.map((col) => col.name);
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ collections: collectionNames }),
+      };
+    } catch (error) {
+      console.error("Error retrieving collections:", error);
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          message: "Failed to fetch collections",
+          error: error.message,
+        }),
+      };
+    } finally {
+      if (client) {
+        await client.close();
+      }
+    }
+  } else if (requestType === "connectLambda") {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "This is the connectLambda response!" }),
+    };
+  } else {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: "Invalid request type" }),
+    };
+  }
 };
