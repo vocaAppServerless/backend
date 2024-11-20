d28a7f15950912710c164efb98d99ae750e61ae1
nurdworker <nurdworker@gmail.com>
2024-11-20 20:04:12 +0900

    connect db test func

--- src/handlers/test.js
+++ src/handlers/test.js
@@ -1,4 +1,8 @@
 const { MongoClient } = require("mongodb");
+const {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} = require("@aws-sdk/client-secrets-manager");
 require("dotenv").config();
 
 const env = process.env.ENV;
@@ -13,47 +17,9 @@ let client;
 
 console.log(env, "여기");
 
-//------
-// const {
-//   SecretsManagerClient,
-//   GetSecretValueCommand,
-// } = require("@aws-sdk/client-secrets-manager");
-
-// const secretName = "eng_voca/mongodb"; // 가져올 비밀 이름
-// const region = "ap-northeast-2"; // AWS 리전
-
-// const client = new SecretsManagerClient({ region });
-
-// const getSecretValue = async () => {
-//   try {
-//     // 비밀을 가져오는 명령어 실행
-//     const data = await client.send(
-//       new GetSecretValueCommand({
-//         SecretId: secretName,
-//         VersionStage: "AWSCURRENT", // 최신 버전의 비밀을 가져옵니다.
-//       })
-//     );
-
-//     // 비밀 값이 문자열로 제공되면 JSON으로 파싱
-//     if (data.SecretString) {
-//       const secret = JSON.parse(data.SecretString); // 문자열을 JSON으로 파싱
-//       console.log("Secret:", secret);
-//       return secret;
-//     } else {
-//       // 바이너리 데이터일 경우 Base64로 디코딩 후 처리
-//       const buff = Buffer.from(data.SecretBinary, "base64");
-//       const secret = JSON.parse(buff.toString("ascii")); // 바이너리를 문자열로 변환 후 JSON 파싱
-//       return secret;
-//     }
-//   } catch (error) {
-//     return error;
-//   }
-// };
-
 // 실행
 
 const getSecretValue = async (client, secretName) => {
-  const { GetSecretValueCommand } = require("@aws-sdk/client-secrets-manager");
   try {
     const data = await client.send(
       new GetSecretValueCommand({
@@ -83,11 +49,8 @@ const chooseDbUri = async () => {
   try {
     if (env == "dev") {
       dbUri = `mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@host.docker.internal:${process.env.MONGODB_PORT}/${process.env.MONGODB_DB}`;
-      return secret;
+      return dbUri;
     } else {
-      const {
-        SecretsManagerClient,
-      } = require("@aws-sdk/client-secrets-manager");
       const secretName = "eng_voca/mongodb";
       const region = "ap-northeast-2";
       client = new SecretsManagerClient({ region });
@@ -96,11 +59,11 @@ const chooseDbUri = async () => {
       dbUri = `mongodb://${secret.MONGODB_USER}:${encodeURIComponent(
         secret.MONGODB_PASSWORD
       )}@${secret.MONGODB_HOST}:${secret.MONGODB_PORT}/${secret.MONGODB_DB}`;
-      return secret;
+      return dbUri;
     }
   } catch (error) {
     console.error("Error in chooseDbUri: ", error);
-    return error;
+    return error.message;
   }
 };
 
@@ -146,7 +109,7 @@ exports.handler = async (event) => {
   } else {
     return {
       statusCode: 200,
-      body: JSON.stringify(await chooseDbUri()),
+      body: JSON.stringify(dbUri),
     };
   }
 };
