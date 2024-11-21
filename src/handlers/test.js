0a408bdb27bf9b6ffd19f26dfd621b8f400757e6
nurdworker <nurdworker@gmail.com>
2024-11-21 17:54:32 +0900

    return fix cors code

--- src/handlers/test.js
+++ src/handlers/test.js
@@ -17,12 +17,7 @@ let client;
 
 console.log(env, "여기");
 
-const headers = {
-  "Access-Control-Allow-Origin": "*", // 모든 도메인 허용 (보안을 위해 특정 도메인으로 제한 가능)
-  "Access-Control-Allow-Methods": "GET, POST, OPTIONS", // 허용할 HTTP 메서드
-  "Access-Control-Allow-Headers":
-    "Content-Type, Authorization, X-Amz-Date, X-Api-Key", // 허용할 헤더들
-};
+
 
 // 실행
 
@@ -92,7 +87,6 @@ exports.handler = async (event) => {
       return {
         statusCode: 200,
         body: JSON.stringify({ collections: collectionNames }),
-        headers,
       };
     } catch (error) {
       console.error("Error retrieving collections:", error);
@@ -101,7 +95,6 @@ exports.handler = async (event) => {
         body: JSON.stringify({
           message: "Failed to fetch collections",
         }),
-        headers,
       };
     } finally {
       if (client) {
@@ -112,13 +105,10 @@ exports.handler = async (event) => {
     return {
       statusCode: 200,
       body: JSON.stringify({ message: "This is the connectLambda response!" }),
-      headers,
     };
   } else {
     return {
       statusCode: 400,
       body: JSON.stringify({ message: "inavailable" }),
-      headers,
     };
-  }
 };
